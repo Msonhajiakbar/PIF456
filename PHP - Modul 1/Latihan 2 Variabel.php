dcaddb563eb48efaab7aad950ae092cd62d20911
Muhammad Sonhaji Akbar <mson.akbar@gmail.com>
2013-10-13 15:17:00 +0700

    PHP - Modul 1
    M. Sonhaji Akbar
    110533430512
    S1 PTI OFF B 2011

---
+++ PHP - Modul 1/Latihan 2 Variabel.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" lang="en">
+<head>
+	<title>Demo Variabel</title>
+</head>
+
+<body>
+<?php
+
+$bil = 3;
+//var dump($bil);
+
+//$var = "";
+var dump($bil);
+
+//$var = null;
+//var dump(&var);
+
+print r($bil);
+?>
+
+
+</body>
+</html>
---
+++ PHP - Modul 1/Latihan 3.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en" lang="en">
+<head>
+	<title>Cek Tipe</title>
+</head>
+
+<body>
+<?php
+
+$bil = 3;
+var dump(is_int($bil));
+//Output: bool(true)
+
+$var = "";
+var dump(is_string($var));
+//Output: bool(true)
+
+print r($bil);
+?>
+
+
+</body>
+</html>
